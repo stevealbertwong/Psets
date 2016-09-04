e3362f2935d6dd2d23f91d201666e0139c916635
steveandrewwong <wongkwunkit@gmail.com>
2016-09-04 01:18:39 -0700

    106l stream manipulator

--- 106l/ch3-stream-manupulator/ch3-stream-manupulator/main.cpp
+++ 106l/ch3-stream-manupulator/ch3-stream-manupulator/main.cpp
@@ -8,28 +8,74 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip> // cout << setw(10)
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+// Global varaible could be called by any function
+const int NUM_LINES = 4;
+const int NUM_COLUMNS = 3;
+const int COLUMN_WIDTH = 20;
 
-    ifstream input("table-data.txt");
+// print dashes and plus
+void PrintTableHeaderDoubleForLoop(){
+    
+    for (int j =0; j<NUM_COLUMNS; j++) {
+        for (int i =0; i <COLUMN_WIDTH; i++) {
+            cout << "-" ;
+        }
+        cout << "+" ;
+    }
+    cout << endl;
+
+    
+};
+
+
+void PrintTableHeaderSetfill(){
+    for (int i = 0; i <NUM_COLUMNS; ++i) {
+        cout << setfill('-') << setw(COLUMN_WIDTH +1) << "+";
+    }
+    cout << setfill(' ') << endl;
+}
+
+// parse lines and words from ifstream
+void PrintTableBody(string filename){
+    
+    ifstream input(filename.c_str());
     if (!input.is_open()){
         cerr << "failed to open file" << endl;
     }
     if (input.is_open()) {
-        cout << "succeeded in opening file" << endl;
+        // cout << "succeeded in opening file" << endl;
     }
     
-    PrintTableHeader();
-    PrintTableBody();
+    
+    // parse words + cout -> for loop 3 times as 3 lines
+    int abc = 0;
+    for (int i=0; i<NUM_LINES; ++i) {
+        int intValue;
+        double doubleValue;
+        input >> intValue >> doubleValue;
+        cout
+        <<setw(COLUMN_WIDTH) << abc+1 << "|"
+        <<setw(COLUMN_WIDTH) << intValue << "|"
+        <<setw(COLUMN_WIDTH) << doubleValue << "|" << endl;
+        abc++;
+    }
+    
+    
+    
+};
+
+
+int main(int argc, const char * argv[]) {
+    
+    string filename1 = "table-data.txt";
+    
+    // PrintTableHeaderDoubleForLoop();
+    PrintTableHeaderSetfill();
+    PrintTableBody(filename1);
 
     return 0;
 }
 
-void PrintTableHeader(){
-    
-};
-
-void PrintTableBody(){
-    
-};
