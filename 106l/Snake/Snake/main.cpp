80437018a2b34931a63c93e8646a97e68ddeee7f
steveandrewwong <wongkwunkit@gmail.com>
2016-09-04 23:17:59 -0700

    snake loadworld

---
+++ 106l/Snake/Snake/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  Snake
+//
+//  Created by Steven Wong on 9/4/16.
+//  Copyright © 2016 Steven Andrew Wong. All rights reserved.
+//
+
+#include <iostream>
+
+// bundle all game data so we could easily pass in as 1 parameter
+struct gameT {
+    
+};
+
+void printBoard(){
+    
+}
+
+void performAI(){
+    
+}
+
+void initializeBoard(){
+    
+
+    
+}
+
+void runSimulation(){
+    
+    printBoard();
+    performAI();
+}
+
+int main(int argc, const char * argv[]) {
+    
+    initializeBoard(); // open txt + getline/ifstream >> int var
+    runSimulation();
+    
+
+    return 0;
+}
