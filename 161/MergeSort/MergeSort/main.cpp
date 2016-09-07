87a607394a92e5a580bf09d1e2c242d450c5065c
steveandrewwong <wongkwunkit@gmail.com>
2016-09-07 07:22:25 -0700

    mergesort c++ code implementation

--- 161/MergeSort/MergeSort/main.cpp
+++ 161/MergeSort/MergeSort/main.cpp
@@ -21,14 +21,58 @@ void mergeSort(int* arr, int leftHead, int length){
         int lh = leftHead; // 0
         int len = length/2;// 4
         int rh = lh + len; // 4
-        cout << "leftHead: " << lh << "length:" << len << "rightHead:" << rh << endl;
+        cout << "leftHead: " << lh <<  "rightHead:" << rh << "length:" << len << endl;
         
-        mergeSort(arr, lh, len); // recursion this one first -> stack 
+        mergeSort(arr, lh, len); // recursion this one first -> stack
         mergeSort(arr, rh, len);
+
+        cout << "this is process :  " <<  "leftHead: " << lh <<  "rightHead:" << rh << "length:" << len << endl;
+        
+        
+        
+        
+        
+        // there are 8 sets of lh, len and rh at this point
+        int temp [len*2]; // you are dealing with your own set of len here
+        
+        int lhCopy = lh;
+        int rhCopy = rh;
+        for (int i =0; i < len*2; i++) {
+            
+            // if both arrays are in bounds
+            if (lh < lhCopy+len && rh < rhCopy+len){
+                // if left smaller
+                if (min(arr[lh], arr[rh])== arr[lh]){
+                    temp[i] = arr[lh];
+                    lh++; // go to the next left
+                // if right smaller
+                } else if (min(arr[lh], arr[rh])== arr[rh]){
+                    temp[i] = arr[rh];
+                    rh++;
+                }
+            // however if anyone goes out of bounds -> just append all of the other array
+            } else if (lh >= lhCopy +len || rh >= rhCopy + len){
+                if (lh >= lhCopy +len) {
+                    temp[i] = arr[rh];
+                    rh++;
+                } else if (rh >= rhCopy + len){
+                    temp[i] = arr[lh];
+                    lh++;
+                }
+            }
+            cout << "this is temp" << temp[i] << endl;
+        }
+        
+        // replace the real array with temp array that stores sorted numbers
+        for (int i = 0; i<len*2; ++i) {
+            arr[lhCopy] = temp[i];
+            // cout << temp[i] <<endl;
+            lhCopy++;
+        }
+        
     }
+    
 }
-
-
 
 
 int main(int argc, const char * argv[]) {
@@ -38,5 +82,9 @@ int main(int argc, const char * argv[]) {
     int length = 8;
     
     mergeSort(arr, leftHead, length);
+
+    for (int i = 0; i<8; ++i) {
+        cout<< arr[i]<< endl;
+    }
     return 0;
 }
