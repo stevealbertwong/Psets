c2e9968fb974fc591f6ae279d430846e5bb5afe4
steveandrewwong <wongkwunkit@gmail.com>
2016-09-07 10:36:37 -0700

    linear regression gradient descent review

--- 229/machine-learning-ex1/ex1/gradientDescent.m
+++ 229/machine-learning-ex1/ex1/gradientDescent.m
@@ -20,8 +20,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    theta_constant = theta(1) - alpha/m * sum(X * theta - y);
-    theta_slope = theta(2) - alpha/m * sum((X * theta - y).*X(:, 2));
+    theta_constant = theta(1) - alpha/m * sum(X * theta - y); %partial derivative on theta_constant
+    theta_slope = theta(2) - alpha/m * sum((X * theta - y).*X(:, 2)); % partial derivative on on theta_slope
     theta = [theta_constant; theta_slope];
     
     
